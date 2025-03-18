4523c72f610d1978c21c0b202c0e9a09a82d938a
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-03-18 02:42:30 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Hero from "@/app/hero/page";
 import { Star, MapPin, Users } from "lucide-react";
 import YachtSearchForm from "@/app/citas/page";
 import Footer from "@/app/footer/page";
-import Services from "@/components/service/page";
+//import Services from "@/components/service/page";
 import Contact from "@/app/contact/page";
 import ChatWidget from "@/components/chat/page";
 
@@ -116,7 +116,7 @@ export default function Home() {
           ))}
         </div>
       </main>
-      <Services services={servicesData} />
+      
       <ChatWidget />
       <Contact />
       <Footer />
